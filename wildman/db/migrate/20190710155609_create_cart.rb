3d1d484d775163f990f07fd5d9f099a19ecd240f
Joshua Crum <joshua.crum@gmail.com>
2019-07-10 15:59:43 +0000

    Updated migrations.

--- wildman/db/migrate/20190710155609_create_cart.rb
+++ wildman/db/migrate/20190710155609_create_cart.rb
@@ -1,4 +1,9 @@
 class CreateCart < ActiveRecord::Migration
   def change
+    create_table :cart do |t|
+      t.integer :user_id
+      t.integer :item_id
+      t.timestamps null: false
+    end
   end
 end
